3372533e57e6bb0c8bdedf1250e0e1a80da71d2c
Disha <dishatalreja1202@gmail.com>
2022-01-07 17:30:18 +0530

    Revert "Added vue 3 support as beforeDestroy hook is deprecated"
    This reverts commit 6968b0669770737900fde81511ffc92f0ac255f6.

--- src/components/ImageBarcodeReader.vue
+++ src/components/ImageBarcodeReader.vue
@@ -1,39 +1,43 @@
 <template>
-  <input
-    @change="onChangeInput"
-    type="file"
-    name="image"
-    accept="image/*"
-    capture="environment"
-  />
+    <input
+        @change="onChangeInput"
+        type="file"
+        name="image"
+        accept="image/*"
+        capture="environment"
+    />
 </template>
 
 <script>
 import { BrowserMultiFormatReader } from "@zxing/library";
+
 export default {
-  name: "image-barcode-reader",
-  data() {
-    return {
-      codeReader: new BrowserMultiFormatReader()
-    };
-  },
-  methods: {
-    onChangeInput(e) {
-      const files = e.target.files || e.dataTransfer.files;
-      if (!files.length) return;
-      const reader = new FileReader();
-      reader.onload = this.processFile;
-      reader.readAsDataURL(files[0]);
+    name: "image-barcode-reader",
+
+    data() {
+        return {
+            codeReader: new BrowserMultiFormatReader()
+        };
     },
 
-    processFile(e) {
-      this.$el.innerHTML += `<img id="image" src="${e.target.result}"/>`;
+    methods: {
+        onChangeInput(e) {
+            const files = e.target.files || e.dataTransfer.files;
+            if (!files.length) return;
 
-      this.codeReader
-      .decodeFromImage("image")
-      .then(result => this.$emit("decode", result.text))
-      .catch(error => this.$emit("error", error));
+            const reader = new FileReader();
+            reader.onload = this.processFile;
+            reader.readAsDataURL(files[0]);
+        },
+
+        processFile(e) {
+            this.$el.innerHTML += `<img id="image" src="${e.target.result}"/>`;
+
+            this.codeReader
+                .decodeFromImage("image")
+                .then(result => this.$emit("decode", result.text))
+                .catch(error => this.$emit("error", error));
+        }
     }
-  }
 };
 </script>
